ed63d3df0d0ace916f9c553a6cf4890c51723603
mgmalana <mgmalana@gmail.com>
2017-03-16 17:21:21 +0800

    Initial emergency page

--- pasada-mobile/src/app/app.component.ts
+++ pasada-mobile/src/app/app.component.ts
@@ -9,7 +9,7 @@ import { LoginPage } from '../pages/login/login';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = LoginPage;
+  rootPage = HomePage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- pasada-mobile/src/app/app.module.ts
+++ pasada-mobile/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { EmergencyPage } from '../pages/emergency/emergency'
 
 import {AuthService} from '../providers/auth-service';
 
@@ -11,6 +12,7 @@ import {AuthService} from '../providers/auth-service';
     MyApp,
     HomePage,
     LoginPage,
+    EmergencyPage,
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,6 +22,7 @@ import {AuthService} from '../providers/auth-service';
     MyApp,
     HomePage,
     LoginPage,
+    EmergencyPage,
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, AuthService]
 })
